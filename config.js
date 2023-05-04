501f7b0a331943c1959655fd9d3d0acfc1ed75ae
tobechinw <69343689+tobechinw@users.noreply.github.com>
2023-05-03 23:18:11 -0400

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGODB: 'mongodb+srv://tobechinwachukwu:yAyB0vU5TYOFhrHa@cluster0.sbwko.mongodb.net/?retryWrites=true&w=majority',
-    SECRET_KEY: 'some very secret key'
-}
