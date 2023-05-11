84f096abea6e550776be3e075f27faf70bdd6231
Tobechi <tobechid6@gmail.com>
2023-05-11 12:13:38 -0400

    bug fixes

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static('public'));
 app.set("view engine", "ejs")
 app.set("view engine", "pug")
-app.use('/views', express.static(path.join(__dirname, 'src', 'views')));
+app.use('/views', express.static('/src/views'));
 app.use('/css', express.static("css"))
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
