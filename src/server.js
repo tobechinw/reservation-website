595497679e32f3f007fba9c2665b330a623ab293
Tobechi <tobechid6@gmail.com>
2023-05-11 13:07:29 -0400

    deployment fixes

--- src/server.js
+++ src/server.js
@@ -10,7 +10,8 @@ app.use(express.static('public'));
 app.set("view engine", "ejs")
 app.set("view engine", "pug")
 
-app.use('/views', express.static("./src/views"));
+app.use('/views', express.static(path.join(__dirname, 'views')));
+console.log(path.join(__dirname, 'views'))
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
