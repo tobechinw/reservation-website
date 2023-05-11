1778934296a03e926f7c57cc0583e5015f1b7838
Tobechi <tobechid6@gmail.com>
2023-05-11 12:28:39 -0400

    deployment fixes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/', require('#base/baseController'))
-router.use('/employees', require("#employees/employeesController"))
+router.use('/employees', require('../employees/employeesController'))
+// router.use('/employees', require("#employees/employeesController"))
 router.use('/rooms', require("#rooms/roomsController"))
 router.use('/reservations', require("#reserve/reserveController"))
 
