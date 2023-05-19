e1f9b874bc7ac57612252e408e95de01e0d3b11a
Tobechi <tobechid6@gmail.com>
2023-05-19 14:58:10 -0400

    routes for authentication and authorization

--- src/server.js
+++ src/server.js
@@ -16,5 +16,5 @@ app.use('/', require('./routes/index.js'))
 const port = process.env.PORT || 3000
 
 app.listen(port, ()=>{
-    console.log(`listening on http://localhost:${port}`)
+    console.log(`listening on port ${port}`)
 })
